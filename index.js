7c2fd8080a1155dcddd4b14ee5a36e017f205861
Abhimanyu Kumar <abhimanyu.vaishy@gmail.com>
2021-08-22 11:20:15 +0530

    removed comments

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const shell = require('shelljs')
+const shell = require('shelljs');
+
+
 shell.echo('********** change dir ******');
 shell.cd('./packages-build');
 
@@ -6,13 +8,3 @@ shell.echo('********** install packages Start ******');
 shell.exec('node install.js && node install-dev.js && node build.js');
 
 shell.echo('********** install packages End ******');
-
-// shell.echo('********** install dev packages Start ******');
-// shell.exec('node install-dev.js');
-
-// shell.echo('********** install dev packages End ******');
-
-// shell.echo('********** build dev packages Start ******');
-// shell.exec('node build.js');
-
-// shell.echo('********** build dev packages End ******');
